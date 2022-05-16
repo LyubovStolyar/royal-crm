012394846234c0b54ef7aa48a2a1802b11265e3b
liuba <zmeenish@ukr.net>
2022-05-16 14:46:50 +0300

    n

---
+++ controllers/database.js
@@ -0,0 +1,45 @@
+const mysql = require("mysql2");
+const config = require("../config/dev");
+
+
+const pool = mysql.createPool({
+  host: config.DB_HOST,
+  user: config.DB_user,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
+});
+
+async function query (sql, values){
+    const promisePool = pool.promise();
+    return [rows, fields] = await promisePool.query(sql, values);
+}
+
+    
+
+
+// function getConnection(){
+//     return new Promise (function(resolve, reject){
+//         pool.getConnection(function (connErr, connection){
+//             if (connErr) reject (connErr)
+//             else resolve (connection)
+//         })
+//     });
+// }
+
+// function runQuery (){
+// return new Promise (function(resolve, reject){
+//     connection.query(sql, function(sqlErr, result, fields){
+//         if(sqlErr) reject (sqlErr)
+//         else resolve (result)
+//         });
+// });
+    
+// }
+
+module.exports = {
+pool,
+query
+}
---
+++ controllers/products.js
@@ -0,0 +1,27 @@
+const database = require('./database');
+
+module.exports = {
+    addProduct: function (name, desc, price){
+        if (!name || name.length ===0){
+            throw ('Error: name is empty');
+        }
+        pool.getConnection(function (connErr, connection){
+            if(connErr) throw connErr; //not connected
+            const sql = "INSERT INTO products(name, description, price) VALUES(?,?,?);";
+            connection.query(sql, [name, desc, price], function(cqlErr, result,fields) {
+            if (sqlErr) throw sqlErr;
+            console.log(result);
+            });
+        });
+    },
+    productList: function (req,res){
+        pool.getConnection(function (connErr, connection){
+            if (connErr) throw connErr;
+            const sql = "SELECT * FROM products";
+            connection.query(sql, function (sqlErr, result, fields){
+                if (sqlErr) throw sqlErr;
+                res.send(result);
+            });
+        });
+    }
+}
